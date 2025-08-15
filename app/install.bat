2cc87af9f9a87f5c1d4393790d4309cdb338b73b
tonkungth11 <nnvvvton@gmail.com>
2025-08-15 14:55:35 +0700

    update(add react)

---
+++ app/install.bat
@@ -0,0 +1,15 @@
+call npm install 
+call npm init -y
+call npm install -D tailwindcss postcss autoprefixer
+call npm install tailwindcss @tailwindcss/vite
+call npm install @types/node@latest --save-dev
+call npm install -D @tailwindcss/cli
+call npm install @tailwindcss/postcss --save-dev
+@REM call npm install express
+@REM call npm install express --save-dev
+call npm ls @types/node
+call npm ls tailwindcss
+call node -v
+call npm -v
+echo.
+set /p=Press ENTER to exit...

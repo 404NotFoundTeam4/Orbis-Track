1b3300df7da82412ccd37cc67945523b04323608
Pakkapon <Pakkapon2547@gmail.com>
2025-10-18 19:54:54 +0700

    add missing controller

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
-import { sendOtpPayload, loginPayload, TokenDto, verifyOtpPayload, forgotPasswordPayload } from "./auth.schema.js";
+import { sendOtpPayload, loginPayload, TokenDto, verifyOtpPayload, forgotPasswordPayload, AuthRequest, accessTokenPayload, MeDto } from "./auth.schema.js";
 import { authService } from "./auth.service.js";
 import { BaseController } from "../../core/base.controller.js";
 import { BaseResponse } from "../../core/base.response.js";
@@ -48,6 +48,18 @@ export class AuthController extends BaseController {
         await authService.logout(token);
 
         return { message: "Logout successful" };
+    }
+
+    /**
+     * Description: ดึงข้อมูลผู้ใช้ปัจจุบันจาก token
+     * Input : req.user จาก auth middleware
+     * Output : { message: "Fetch me successful", data: meDto }
+     * Author : Pakkapon Chomchoey (Tonnam) 66160080
+     */
+    async fetchMe(req: AuthRequest, res: Response, next: NextFunction): Promise<BaseResponse<MeDto>> {
+        const user = accessTokenPayload.parse(req.user);
+        const result = await authService.fetchMe(user);
+        return { message: "Fetch me successful", data: result };
     }
 
     async sendOtp(req: Request, res: Response, next: NextFunction): Promise<BaseResponse<void>> {

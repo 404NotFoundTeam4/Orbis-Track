fd932adae870c0762105e25505854c5107619995
Pakkapon <Pakkapon2547@gmail.com>
2025-11-16 04:41:31 +0700

    fix error at frontend and seed.ts

--- app/src/middlewares/ProtectedRoute.tsx
+++ app/src/middlewares/ProtectedRoute.tsx
@@ -1,16 +1,17 @@
-import { useEffect  } from "react";
+import { useEffect } from "react";
 import type { ReactElement } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useUserStore } from "../stores/UserStore";
+import { useUserStore } from "../stores/userStore";
 import { useLogin } from "../hooks/useLogin";
 export default function ProtectedRoute(): ReactElement {
-  const { ReloadUser } = useLogin();          
+  const { ReloadUser } = useLogin();
   useEffect(() => {
-    ReloadUser();                            // โหลด user + เช็ค token / exp
-  }, []); 
+    ReloadUser(); // โหลด user + เช็ค token / exp
+  }, []);
   const location = useLocation();
   // Check token in localStorage. If absent -> redirect to login.
-  const token = sessionStorage.getItem("token") || localStorage.getItem("token");
+  const token =
+    sessionStorage.getItem("token") || localStorage.getItem("token");
 
   if (!token) {
     return <Navigate to="/login" replace state={{ from: location }} />;

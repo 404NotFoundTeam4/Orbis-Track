65747d6cf8b6d17a3917beabaac193d395c55970
Pakkapon <Pakkapon2547@gmail.com>
2025-11-16 17:26:29 +0700

    fix env vite api base frontend

--- app/src/api/axios.js
+++ app/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api", // backend port
+  baseURL: import.meta.env.VITE_API_BASE || "/api", // backend port
   withCredentials: true, // ถ้าใช้ cookie auth
 });
 
